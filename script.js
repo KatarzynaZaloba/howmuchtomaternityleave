92f48f22091142bbec36f41015231aec330e6f5b
Katarzyna Zaloba <kzalobafrontend@gmail.com>
2025-07-17 10:06:10 +0200

    add styles and script to the project

---
+++ script.js
@@ -0,0 +1,31 @@
+const daysE1 = document.getElementById('days');
+const hoursE1 = document.getElementById('hours');
+const minutesE1 = document.getElementById('minutes');
+const secondsE1 = document.getElementById('seconds');
+
+const newJob = 'Oct 20 2025';
+
+function countdown() {
+    const newJobDate = new Date(newJob);
+    const currentDate = new Date();
+
+    const totalSeconds = new Date(newJobDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysE1.innerHTML = days;
+    hoursE1.innerHTML = formatTime(hours);
+    minutesE1.innerHTML = formatTime(minutes);
+    secondsE1.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
